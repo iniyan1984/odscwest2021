d8d6aba7e43deba22708e274377e52f109cb6761
Jared P. Lander <jared@landeranalytics.com>
2021-11-15 14:49:58 -0500

    Show smoothing curves

--- code/Plotting.r
+++ code/Plotting.r
@@ -14,3 +14,39 @@ ggplot(diamonds, aes(x=carat, y=price)) + geom_point()
 ggplot(diamonds, aes(x=carat, y=price)) + geom_point(color='red')
 ggplot(diamonds, aes(x=carat, y=price)) + geom_point(aes(color=cut))
 ggplot(diamonds, aes(x=carat, y=price, color=cut)) + geom_point()
+
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point(shape=1)
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point(shape=1, size=1)
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point(shape=1, size=1, alpha=1/3)
+
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point(shape=1, size=1) +
+    scale_color_viridis_d(option='magma')
+
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point(shape=1, size=1) +
+    # scale_color_manual(values=wesanderson::wes_palette(name='Rushmore', n=5))
+    scale_color_manual(
+        values=wesanderson::wes_palette(name='FantasticFox1', n=5)
+    )
+
+# Smoothing Curves ####
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point(shape=1, size=1) + 
+    geom_smooth()
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(aes(color=cut)) + 
+    geom_smooth()
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point() + 
+    geom_smooth(aes(color=cut))
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(aes(color=cut), shape=1, size=1) + 
+    geom_smooth(aes(color=cut), se=FALSE, linetype=2) + 
+    geom_smooth(color='hotpink')
