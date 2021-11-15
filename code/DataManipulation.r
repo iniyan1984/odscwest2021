7023da353ab63cfec89e32700b2b75e3c5bb4d85
Jared P. Lander <jared@landeranalytics.com>
2021-11-15 13:23:25 -0500

    Showed select, slice, filter

--- code/DataManipulation.r
+++ code/DataManipulation.r
@@ -19,6 +19,8 @@ data(diamonds, package='ggplot2')
 
 diamonds
 
+# Pipes ####
+
 # %>%: magrittr
 # |>: base R
 
@@ -35,3 +37,33 @@ tail(head(diamonds, n=4), n=1)
 diamonds |> head(n=4) |> tail(n=1)
 
 # ctrl-shift-M
+
+# Selecting columns ####
+select(diamonds, carat, price)
+diamonds
+
+diamonds |> select(carat, price)
+diamonds |> select('carat', 'price')
+diamonds |> select(c(1, 7))
+diamonds |> select(-carat, -price)
+
+# Getting rows ####
+
+diamonds |> slice(4)
+diamonds |> slice(c(4, 7))
+diamonds |> slice(-c(4, 7))
+
+# Filtering rows ####
+diamonds |> filter(carat >= 1)
+diamonds
+
+diamonds |> filter(carat == 1)
+diamonds |> filter(carat != 1)
+
+diamonds |> filter(cut == 'Ideal')
+diamonds |> filter(cut == 'Ideal' | cut == 'Premium')
+diamonds |> filter(cut == 'Ideal' | cut == 'Premium' | cut == 'Good')
+diamonds |> filter(cut %in% c('Ideal', 'Premium', 'Good'))
+
+diamonds |> filter(carat >= 1 & cut == 'Ideal')
+
